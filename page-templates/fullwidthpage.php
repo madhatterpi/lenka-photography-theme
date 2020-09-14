caf1e978b9212e49d1b8976045f7d4db0bdec304
madhatterpi <peter_bateman@hotmail.co.uk>
2020-09-14 20:36:48 +0100

    before comp upgrade

--- page-templates/fullwidthpage.php
+++ page-templates/fullwidthpage.php
@@ -17,7 +17,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <div class="wrapper" id="full-width-page-wrapper">
 
-	<div class="<?php echo esc_attr( $container ); ?>" id="content">
+	<div class="container-fluid" id="content">
 
 		<div class="row">
 
